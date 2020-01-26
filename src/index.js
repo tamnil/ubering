135f15011d7cbe86d73351a45ef0ee3dcaafa664
Tamnil Saito Junior <tamnil@gmail.com>
2020-01-26 11:05:23 -0300

    converted to module

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const uber = require("./services/uberService");
 
-const origin = [-22.8745132, -47.0494196],
-    dest = [-22.9088671, -47.0553481];
+console.log(uber)
 
-uber.getFareEstimates(origin,dest).then((res)=>console.log(res.data));
+module.exports = uber
