8f05bc2b69ad036e04ceec511ca0ef288734b2dc
Tamnil Saito Junior <tamnil@gmail.com>
2020-01-26 15:53:45 -0300

    sketching examples

---
+++ examples/getPriceEstimation.js
@@ -0,0 +1,20 @@
+const readline = require('readline');
+const uber = require('../src/index')
+
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let questionOrigin = rl.question('Origin:', (answer) => {
+  console.log(`quering uber`);
+  rl.close();
+});
+
+let questionDestination = rl.question('Destination:', (answer) => {
+  console.log(`quering uber`);
+  rl.close();
+});
+
+// uber.getUuid().then(res => console.log(res))
